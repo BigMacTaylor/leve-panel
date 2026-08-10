BigMacTaylor/leve-panel

commit 447a62e2cd441b16a1809ebe903b1eef2b7cc583

add mem and cpu widgets

src/leve-panel/panel.nim
@@ -77,41 +77,31 @@ proc roundBgCorners(ctx: Context, side: Side, width, height: int32) =
   ctx.fillStyle = p.color
   ctx.fill()
 
+# ----------------------------------------------------------------------------------------
+#                                    Update Widgets
+# ----------------------------------------------------------------------------------------
+
 proc updateWidget(w: ptr Widget) =
   let width = int32(w.endPos[0] - w.startPos[0])
   let height = int32(w.endPos[1] - w.startPos[1])
 
-  # Create new widget image
-  let newImgData = newImage(width, height)
-  let ctx = newImgData.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    newImgData.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, width, height)
-
-  # Draw widget icon
-  ctx.drawImage(w.img, 0, 0)
-
   # Copy new image to image data
-  var dataPos = 0
-  var newDataPos = 0
+  let panelStride = p.width
+  var panelRowStart = w.startPos[1] * panelStride + w.startPos[0]
+  var widgetRowStart = 0
+  let rowBytes = width * 4
 
-  if p.pos == top or p.pos == bottom:
-    dataPos = w.startPos[0]
-    for i in 0 ..< height:
-      copyMem(p.pixelData[dataPos].addr, newImgData.data[newDataPos].addr, width * 4)
-      dataPos = dataPos + p.width
-      newDataPos = newDataPos + width
-  else:
-    dataPos = w.startPos[1] * width
-    for i in w.startPos[1] ..< w.endPos[1]:
-      copyMem(p.pixelData[dataPos].addr, newImgData.data[newDataPos].addr, width * 4)
-      dataPos = dataPos + width
-      newDataPos = newDataPos + width
+  for i in 0 ..< height:
+    copyMem(
+      p.pixelData[panelRowStart].addr,
+      w.img.data[widgetRowStart].addr, 
+      rowBytes
+    )
+    panelRowStart += panelStride
+    widgetRowStart += width
 
   # Attach and Damage
+  # TODO fix wl_surface_attach
   p.surface.wl_surface_attach(p.buffer, 0, 0)
   p.surface.wl_surface_damage(int32(w.startPos[0]), int32(w.startPos[1]), width, height)
 
@@ -135,6 +125,12 @@ proc createWidget(item: PanelItem, pos: float32): Widget =
     widget = newPowerWidget(item, pos)
   of WidgetType.desktop:
     widget = newDesktopWidget(item, pos)
+  of WidgetType.cpu:
+    widget = newCpuWidget(item, pos)
+  of WidgetType.mem:
+    widget = newMemWidget(item, pos)
+  else:
+    discard
 
   return widget
 

src/leve-panel/widgets/clock.nim
@@ -22,12 +22,15 @@ proc onClock(data: pointer) =
   echo "open clock widget"
   exec(cast[ptr PanelItem](data))
 
-proc newClockImg(): Image =
-  let img = 
-    if p.pos == top or p.pos == bottom:
-      newImage(p.size * 2, p.size)
-    else:
-      newImage(p.size, p.size * 2)
+proc drawClockImg(w: ptr Widget) =
+  let ctx = w.img.newContext()
+
+  # Draw widget background
+  if w.roundedSide == none:
+    w.img.fill(p.color)
+  else:
+    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
+
   let text = getTime()
 
   # Draw Text
@@ -42,16 +45,13 @@ proc newClockImg(): Image =
   # Center text both horizontally and vertically
   let layout = font.typeset(
     text,
-    bounds = vec2(img.width.float, img.height.float),
+    bounds = vec2(w.img.width.float, w.img.height.float),
     hAlign = CenterAlign,  # Horizontal: Left, Center, Right
     vAlign = MiddleAlign   # Vertical: Top, Middle, Bottom
   )
 
   # Draw the text within the specified bounds, centered
-  img.fillText(layout, translate(vec2(0, 0)))
-  #clock.fillText(font.typeset(text, vec2(180, 180)), translate(vec2(0, 0)))
-
-  return img
+  w.img.fillText(layout, translate(vec2(0, 0)))
 
 proc newClockWidget(i: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
@@ -66,8 +66,12 @@ proc newClockWidget(i: PanelItem, pos: float32): Widget =
     else:
       [int(p.size), int(pos) + (2 * p.size)]
 
-  # Create Clock Image
-  let img = newClockImg()
+  # Create Image
+  let img = 
+    if p.pos == top or p.pos == bottom:
+      newImage(p.size * 2, p.size)
+    else:
+      newImage(p.size, p.size * 2)
 
   # Create callbacks
   let click: CallBack = (Event.click_l, proc(data: pointer) = onClock(addr i))
@@ -75,6 +79,7 @@ proc newClockWidget(i: PanelItem, pos: float32): Widget =
 
   # Create widget
   let widget: Widget = Widget(widgetType: clock, startPos: startPos, endPos: endPos, img: img, callBacks: callBacks)
+  drawClockImg(addr widget)
 
   return widget
 

src/leve-panel/widgets/cpu.nim
@@ -1,0 +1,157 @@
+# ========================================================================================
+#
+#                                   Leve Panel
+#                                   CPU Widget
+#
+# ========================================================================================
+
+type CpuTime = object
+  user, nice, system, idle, iowait, irq, softirq, steal: int
+
+var cpuTime: CpuTime
+
+proc newFont(typeface: Typeface, size: float32, color: Color): Font =
+  result = newFont(typeface)
+  result.size = size
+  result.paint.color = color
+
+proc getCpuTime(): CpuTime =
+  # Reads the first line of /proc/stat
+  let lines = readFile("/proc/stat").splitLines()
+  if lines.len == 0: return
+  
+  # The first line starts with "cpu " followed by the metrics
+  let fields = lines[0].splitWhitespace()
+  if fields.len < 9 or fields[0] != "cpu":
+    raise newException(ValueError, "Failed to parse /proc/stat")
+
+  result.user    = parseInt(fields[1])
+  result.nice    = parseInt(fields[2])
+  result.system  = parseInt(fields[3])
+  result.idle    = parseInt(fields[4])
+  result.iowait  = parseInt(fields[5])
+  result.irq     = parseInt(fields[6])
+  result.softirq = parseInt(fields[7])
+  result.steal   = parseInt(fields[8])
+
+proc calculateCpuPercent(t1, t2: CpuTime): float =
+  # Calculate totals
+  let prevIdle = t1.idle + t1.iowait
+  let idle = t2.idle + t2.iowait
+
+  let prevNonIdle = t1.user + t1.nice + t1.system + t1.irq + t1.softirq + t1.steal
+  let nonIdle = t2.user + t2.nice + t2.system + t2.irq + t2.softirq + t2.steal
+
+  let prevTotal = prevIdle + prevNonIdle
+  let total = idle + nonIdle
+
+  # Diffs
+  let totalDiff = total - prevTotal
+  let idleDiff = idle - prevIdle
+
+  if totalDiff == 0: return 0.0
+  
+  # Percentage formula
+  result = (totalDiff - idleDiff).float / totalDiff.float * 100.0
+
+# Callback function to update the label
+proc getCpuText(): string =
+  var usage: float
+  try:
+    let t1 = cpuTime
+    let t2 = getCpuTime()
+    cpuTime = t2
+
+    usage = calculateCpuPercent(t1, t2)
+    echo "Current CPU Usage: ", usage.formatFloat(ffDecimal, 2), "%"
+  except Exception as e:
+    echo "Error: ", e.msg
+
+  let num = usage.toInt()
+
+  return "CPU\n" & "\xA0\xA0" & $num & "%"
+
+proc getCpuTextBak(cmd: string): string =
+  let now = now()
+  let time = now.format("h:mm tt")
+  let date = now.format("MM/d/YYYY")
+
+  let cmd = """vmstat 1 2 | tail -1 | awk '{print $15}' """
+  let (output, status) = execCmdEx(cmd)
+
+  if status != 0:
+    echo "Error: Could not get mute status"
+    return "Error"
+
+  var num = output.strip.parseInt
+  num = num div 10
+
+
+  return $num & "%"
+
+proc onCpuBtn(data: pointer) =
+  echo "power off menu "
+  exec(cast[ptr PanelItem](data))
+
+proc drawCpuImg(w: ptr Widget) =
+  let ctx = w.img.newContext()
+
+  # Draw widget background
+  if w.roundedSide == none:
+    w.img.fill(p.color)
+  else:
+    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
+
+  let iconSize = if p.iconSize > 24:
+    p.iconSize - 2
+  else:
+    p.iconSize
+
+  let padding = (p.size - p.iconSize) / 2
+
+  # Draw Text
+  let text = getCpuText()
+  let font = try:
+    readFont(fontPath)
+  except:
+    fontPath = getFont()
+    readFont(fontPath)
+  font.size = 14
+  font.paint.color = color(1, 1, 1) # White
+
+  # Center text both horizontally and vertically
+  let layout = font.typeset(
+    text,
+    bounds = vec2(w.img.width.float, w.img.height.float),
+    hAlign = CenterAlign,  # Horizontal: Left, Center, Right
+    vAlign = MiddleAlign   # Vertical: Top, Middle, Bottom
+  )
+
+  # Draw the text within the specified bounds, centered
+  w.img.fillText(layout, translate(vec2(0, 0)))
+
+proc newCpuWidget(i: PanelItem, pos: float32): Widget =
+  let startPos: array[2, int] =
+    if p.pos == top or p.pos == bottom:
+      [int(pos), 0]
+    else:
+      [0, int(pos)]
+
+  let endPos: array[2, int] =
+    if p.pos == top or p.pos == bottom:
+      [int(pos) + int(p.size), int(p.size)]
+    else:
+      [int(p.size), int(pos) + int(p.size)]
+
+  # Create Image
+  let img = newImage(p.size, p.size)
+
+  # Create callbacks
+  let click: CallBack = (Event.click_l, proc(data: pointer) = onCpuBtn(addr i))
+  let callBacks: seq[CallBack] = @[click]
+
+  # Create widget
+  var widget: Widget = Widget(widgetType: cpu, startPos: startPos, endPos: endPos, roundedSide: Side.none, img: img, callBacks: callBacks)
+  drawCpuImg(addr widget)
+
+  return widget

commit e48e35b436b3d40d6314ac190d976036a7b341d9

fix yet another regression from commit 447a62e

src/leve-panel/panel.nim
@@ -77,6 +77,16 @@ proc roundBgCorners(ctx: Context, side: Side, width, height: int32) =
   ctx.fillStyle = p.color
   ctx.fill()
 
+proc clearBgImg(w: ptr Widget) =
+  let ctx = w.img.newContext()
+
+  # Draw widget background
+  if w.roundedSide == none:
+    w.img.fill(p.color)
+  else:
+    w.img.fill(rgba(0, 0, 0, 0))
+    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
+
 # ----------------------------------------------------------------------------------------
 #                                    Update Widgets
 # ----------------------------------------------------------------------------------------
@@ -107,26 +117,26 @@ proc updateWidget(w: ptr Widget) =
 #                                    Create Widgets
 # ----------------------------------------------------------------------------------------
 
-proc createWidget(item: PanelItem, pos: float32, rdSide: Side): Widget =
+proc createWidget(item: PanelItem, pos: float32): Widget =
   var widget: Widget
 
   case item.widget
   of WidgetType.favorite:
-    widget = newFavWidget(item, pos, rdSide)
+    widget = newFavWidget(item, pos)
   of WidgetType.clock:
-    widget = newClockWidget(item, pos, rdSide)
+    widget = newClockWidget(item, pos)
   of WidgetType.volume:
-    widget = newVolWidget(item, pos, rdSide)
+    widget = newVolWidget(item, pos)
   of WidgetType.menu:
-    widget = newMenuWidget(item, pos, rdSide)
+    widget = newMenuWidget(item, pos)
   of WidgetType.power:
-    widget = newPowerWidget(item, pos, rdSide)
+    widget = newPowerWidget(item, pos)
   of WidgetType.desktop:
-    widget = newDesktopWidget(item, pos, rdSide)
+    widget = newDesktopWidget(item, pos)
   of WidgetType.cpu:
-    widget = newCpuWidget(item, pos, rdSide)
+    widget = newCpuWidget(item, pos)
   of WidgetType.mem:
-    widget = newMemWidget(item, pos, rdSide)
+    widget = newMemWidget(item, pos)
 
   return widget
 
@@ -168,19 +178,17 @@ proc drawPanelImg(panel: ptr Panel): Image =
   # Add Left Widgets
   var endWidget = true
   var pos: float32 = 0
-  var rdSide = none
   debug "get left items"
   let leftItems = panel.getItems("Left")
   for item in leftItems:
+    var widget: Widget = createWidget(item, pos)
+
     if endWidget:
       if panel.pos == top or panel.pos == bottom:
-        rdSide = left
+        widget.roundedSide = left
       else:
-        rdSide = top
-    else: rdSide = none
+        widget.roundedSide = top
     endWidget = false
-
-    var widget: Widget = createWidget(item, pos, rdSide)
 
     widgets.add(widget)
 
@@ -215,9 +223,7 @@ proc drawPanelImg(panel: ptr Panel): Image =
 
   # Add Center Widgets
   for item in centerItems:
-    rdSide = none
-
-    var widget: Widget = createWidget(item, pos, rdSide)
+    var widget: Widget = createWidget(item, pos)
 
     widgets.add(widget)
 
@@ -249,15 +255,14 @@ proc drawPanelImg(panel: ptr Panel): Image =
     elif (item.widget == WidgetType.desktop) and (item.style != num):
       pos = pos - float32(3 * p.size)
 
+    var widget: Widget = createWidget(item, pos)
+
     if endWidget:
       if panel.pos == top or panel.pos == bottom:
-        rdSide = right
+        widget.roundedSide = right
       else:
-        rdSide = bottom
-    else: rdSide = none
+        widget.roundedSide = bottom
     endWidget = false
-
-    var widget: Widget = createWidget(item, pos, rdSide)
 
     widgets.add(widget)
 

src/leve-panel/widgets/clock.nim
@@ -23,14 +23,6 @@ proc onClock(data: pointer) =
   exec(cast[ptr PanelItem](data))
 
 proc drawClockImg(w: ptr Widget) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   let text = getTime()
 
   # Draw Text
@@ -53,7 +45,7 @@ proc drawClockImg(w: ptr Widget) =
   # Draw the text within the specified bounds, centered
   w.img.fillText(layout, translate(vec2(0, 0)))
 
-proc newClockWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
+proc newClockWidget(i: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
     if p.pos == top or p.pos == bottom:
       [int(pos), 0]
@@ -82,7 +74,6 @@ proc newClockWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
     widgetType: clock,
     startPos: startPos,
     endPos: endPos,
-    roundedSide: rdSide,
     img: img,
     callBacks: callBacks,
   )

src/leve-panel/widgets/cpu.nim
@@ -71,14 +71,6 @@ proc onCpuBtn(data: pointer) =
   exec(cast[ptr PanelItem](data))
 
 proc drawCpuImg(w: ptr Widget) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   # Draw Text
   let text = getCpuText()
   let font = try:
@@ -100,7 +92,7 @@ proc drawCpuImg(w: ptr Widget) =
   # Draw the text within the specified bounds, centered
   w.img.fillText(layout, translate(vec2(0, 0)))
 
-proc newCpuWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
+proc newCpuWidget(i: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
     if p.pos == top or p.pos == bottom:
       [int(pos), 0]
@@ -125,7 +117,6 @@ proc newCpuWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
     widgetType: cpu,
     startPos: startPos,
     endPos: endPos,
-    roundedSide: rdSide,
     img: img,
     callBacks: callBacks,
   )

src/leve-panel/widgets/desktop.nim
@@ -6,18 +6,11 @@
 # ========================================================================================
 
 proc hDesktopDotsImg(w: ptr Widget, curWS: int) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   let centerX: float32 = float32(p.size * 2)
   let numCircles: int = getNumWorkspaces()
   let radius: int = p.size div 10
   let color = rgba(255, 255, 255, 255)
+  let ctx = w.img.newContext()
   ctx.fillStyle = color
 
   # Row configuration
@@ -35,18 +28,11 @@ proc hDesktopDotsImg(w: ptr Widget, curWS: int) =
       ctx.fillCircle(circle(vec2(posX, posY), float32(radius)))
 
 proc vDesktopDotsImg(w: ptr Widget, curWS: int) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   let centerX: float32 = float32(p.size * 2)
   let numCircles: int = getNumWorkspaces()
   let radius: int = p.size div 10
   let color = rgba(255, 255, 255, 255)
+  let ctx = w.img.newContext()
   ctx.fillStyle = color
 
   # Row configuration
@@ -64,14 +50,6 @@ proc vDesktopDotsImg(w: ptr Widget, curWS: int) =
       ctx.fillCircle(circle(vec2(posX, posY), float32(radius)))
 
 proc hDesktopNumbersImg(w: ptr Widget, curWS: int) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   var text = ""
 
   for workspace in workspaces:
@@ -101,14 +79,6 @@ proc hDesktopNumbersImg(w: ptr Widget, curWS: int) =
   w.img.fillText(layout, translate(vec2(0, 0)))
 
 proc vDesktopNumbersImg(w: ptr Widget, curWS: int) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   var text = ""
 
   for workspace in workspaces:
@@ -138,14 +108,6 @@ proc vDesktopNumbersImg(w: ptr Widget, curWS: int) =
   w.img.fillText(layout, translate(vec2(0, 0)))
 
 proc desktopNumImg(w: ptr Widget, curWS: int) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   let text = $curWS
 
   # Draw Text
@@ -168,7 +130,7 @@ proc desktopNumImg(w: ptr Widget, curWS: int) =
   # Draw the text within the specified bounds, centered
   w.img.fillText(layout, translate(vec2(0, 0)))
 
-proc newDesktopWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
+proc newDesktopWidget(i: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
     if p.pos == top or p.pos == bottom:
       [int(pos), 0]
@@ -214,7 +176,6 @@ proc newDesktopWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
     widgetType: desktop,
     startPos: startPos,
     endPos: endPos,
-    roundedSide: rdSide,
     img: img,
   )
 

src/leve-panel/widgets/favorites.nim
@@ -89,14 +89,6 @@ proc onFavClick(data: pointer) =
   exec(cast[ptr PanelItem](data))
 
 proc drawFavImg(w: ptr Widget, fav: PanelItem) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   let padding = (p.size - p.iconSize) / 2
 
   # Load Icon
@@ -132,7 +124,7 @@ proc drawFavImg(w: ptr Widget, fav: PanelItem) =
   let sizedIcon = icon.resize(p.iconSize, p.iconSize)
   w.img.draw(sizedIcon, translate(vec2(padding.float32, padding.float32)))
 
-proc newFavWidget(fav: PanelItem, pos: float32, rdSide: Side): Widget =
+proc newFavWidget(fav: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
     if p.pos == top or p.pos == bottom:
       [int(pos), 0]
@@ -157,7 +149,6 @@ proc newFavWidget(fav: PanelItem, pos: float32, rdSide: Side): Widget =
     widgetType: favorite,
     startPos: startPos,
     endPos: endPos,
-    roundedSide: rdSide,
     img: img,
     callBacks: callBacks,
   )

src/leve-panel/widgets/mem.nim
@@ -46,14 +46,6 @@ proc onMemBtn(data: pointer) =
   exec(cast[ptr PanelItem](data))
 
 proc drawMemImg(w: ptr Widget) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   # Draw Text
   let text = getMemText()
   let font = try:
@@ -75,7 +67,7 @@ proc drawMemImg(w: ptr Widget) =
   # Draw the text within the specified bounds, centered
   w.img.fillText(layout, translate(vec2(0, 0)))
 
-proc newMemWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
+proc newMemWidget(i: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
     if p.pos == top or p.pos == bottom:
       [int(pos), 0]
@@ -100,7 +92,6 @@ proc newMemWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
     widgetType: mem,
     startPos: startPos,
     endPos: endPos,
-    roundedSide: rdSide,
     img: img,
     callBacks: callBacks,
   )

src/leve-panel/widgets/menu.nim
@@ -10,14 +10,6 @@ proc onMenuBtn(data: pointer) =
   exec(cast[ptr PanelItem](data))
 
 proc drawMenuImg(w: ptr Widget) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   let iconSize = if p.iconSize > 24:
     p.iconSize - 4
   else:
@@ -43,7 +35,7 @@ proc drawMenuImg(w: ptr Widget) =
   let sizedIcon = icon.resize(iconSize, iconSize)
   w.img.draw(sizedIcon, translate(vec2(padding.float32, padding.float32)))
 
-proc newMenuWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
+proc newMenuWidget(i: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
     if p.pos == top or p.pos == bottom:
       [int(pos), 0]
@@ -68,7 +60,6 @@ proc newMenuWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
     widgetType: menu,
     startPos: startPos,
     endPos: endPos,
-    roundedSide: rdSide,
     img: img,
     callBacks: callBacks,
   )

src/leve-panel/widgets/power.nim
@@ -10,14 +10,6 @@ proc onPowerBtn(data: pointer) =
   exec(cast[ptr PanelItem](data))
 
 proc drawPowerImg(w: ptr Widget) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   let iconSize = if p.iconSize > 24:
     p.iconSize - 6
   else:
@@ -40,7 +32,7 @@ proc drawPowerImg(w: ptr Widget) =
   let sizedIcon = icon.resize(iconSize, iconSize)
   w.img.draw(sizedIcon, translate(vec2(padding.float32, padding.float32)))
 
-proc newPowerWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
+proc newPowerWidget(i: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
     if p.pos == top or p.pos == bottom:
       [int(pos), 0]
@@ -65,7 +57,6 @@ proc newPowerWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
     widgetType: power,
     startPos: startPos,
     endPos: endPos,
-    roundedSide: rdSide,
     img: img,
     callBacks: callBacks,
   )

src/leve-panel/widgets/volume.nim
@@ -86,14 +86,6 @@ proc checkVolStatus() =
 # ----------------------------------------------------------------------------------------
 
 proc drawVolImg(w: ptr Widget) =
-  let ctx = w.img.newContext()
-
-  # Draw widget background
-  if w.roundedSide == none:
-    w.img.fill(p.color)
-  else:
-    ctx.roundBgCorners(w.roundedSide, w.img.width.int32, w.img.height.int32)
-
   let iconSize =
     if p.iconSize > 24:
       p.iconSize - 4
@@ -134,11 +126,11 @@ proc drawVolImg(w: ptr Widget) =
 # ----------------------------------------------------------------------------------------
 
 proc onVolClick(data: pointer) =
-  echo "open volume control"
+  debug "open volume control"
   exec(cast[ptr PanelItem](data))
 
 proc onMute(data: pointer) =
-  echo "on mute"
+  debug "on mute"
   let cmd = "pactl set-sink-mute @DEFAULT_SINK@ "
 
   if volMute:
@@ -158,12 +150,13 @@ proc onMute(data: pointer) =
   volState = getVolState()
 
   p.surface.wl_surface_attach(p.buffer, 0, 0)
+  clearBgImg(cast[ptr Widget](data))
   drawVolImg(cast[ptr Widget](data))
   updateWidget(cast[ptr Widget](data))
   p.surface.wl_surface_commit()
 
 proc volUp(data: pointer) =
-  echo "volume up"
+  debug "volume up"
   let curVolState = volState
 
   # Check bounds
@@ -186,7 +179,7 @@ proc volUp(data: pointer) =
     volMute = false
     let muteCmd = "pactl set-sink-mute @DEFAULT_SINK@ "
     discard execShellCmd(muteCmd & "0")
-  echo "mute state ", volMute
+  debug "mute state ", volMute
 
   # Update tooltip
   tt.text = "Volume: " & $cur_vol & "%"
@@ -199,12 +192,13 @@ proc volUp(data: pointer) =
     return
 
   p.surface.wl_surface_attach(p.buffer, 0, 0)
+  clearBgImg(cast[ptr Widget](data))
   drawVolImg(cast[ptr Widget](data))
   updateWidget(cast[ptr Widget](data))
   p.surface.wl_surface_commit()
 
 proc volDown(data: pointer) =
-  echo "volume down"
+  debug "volume down"
   let curVolState = volState
 
   # Check bounds
@@ -232,6 +226,7 @@ proc volDown(data: pointer) =
     return
 
   p.surface.wl_surface_attach(p.buffer, 0, 0)
+  clearBgImg(cast[ptr Widget](data))
   drawVolImg(cast[ptr Widget](data))
   updateWidget(cast[ptr Widget](data))
   p.surface.wl_surface_commit()
@@ -240,7 +235,7 @@ proc volDown(data: pointer) =
 #                                    Create Widget
 # ----------------------------------------------------------------------------------------
 
-proc newVolWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
+proc newVolWidget(i: PanelItem, pos: float32): Widget =
   let startPos: array[2, int] =
     if p.pos == top or p.pos == bottom:
       [int(pos), 0]
@@ -273,7 +268,6 @@ proc newVolWidget(i: PanelItem, pos: float32, rdSide: Side): Widget =
     widgetType: volume,
     startPos: startPos,
     endPos: endPos,
-    roundedSide: rdSide,
     img: img,
     callBacks: callBacks,
   )

src/leve-panel/workspaces.nim
@@ -94,6 +94,7 @@ proc onWsEventDone(data: pointer, manager: ptr ext_workspace_manager_v1) {.cdecl
   p.surface.wl_surface_attach(p.buffer, 0, 0)
   for widget in widgets:
     if widget.widgetType == WidgetType.desktop:
+      clearBgImg(addr widget)
       drawDesktopImg(addr widget, getCurrentWS())
       updateWidget(addr widget)
   p.surface.wl_surface_commit()

src/leve_panel.nim
@@ -212,7 +212,7 @@ var tt = Tooltip()
 var m = Menu()
 setCurrentDir(getHomeDir())
 
-proc roundBgCorners(ctx: Context, side: Side, width, height: int32)
+proc clearBgImg(w: ptr Widget)
 proc updateWidget(w: ptr Widget)
 proc updateTooltip(tooltip: ptr Tooltip)
 proc createPopup(w: Widget, data: pointer)
@@ -494,22 +494,27 @@ proc main() =
         case widget.widgetType
         of WidgetType.clock:
           if updateClock:
+            clearBgImg(addr widget)
             drawClockImg(addr widget)
             updateWidget(addr widget)
         of WidgetType.cpu:
           if updateStats:
+            clearBgImg(addr widget)
             drawCpuImg(addr widget)
             updateWidget(addr widget)
         of WidgetType.mem:
           if updateStats:
+            clearBgImg(addr widget)
             drawMemImg(addr widget)
             updateWidget(addr widget)
         of WidgetType.volume:
           if updateVol:
+            clearBgImg(addr widget)
             drawVolImg(addr widget)
             updateWidget(addr widget)
         of WidgetType.desktop:
           if updateDesktop:
+            clearBgImg(addr widget)
             drawDesktopImg(addr widget, curWS)
             updateWidget(addr widget)
             timeOut = -1